deec85f495a351098820f012883361f27b5a93c9
OneGoodReas0n <korobovi4@gmail.com>
2019-10-07 21:51:21 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  let wordCount = String(str).length
+  for (let j = 0; j < bracketsConfig.length; j++) {
+    let i = 0;
+    while (i < String(str).length) {
+      if (str[i] === bracketsConfig[j][0] && str[i + 1] === bracketsConfig[j][1]) {
+        str = String(str).slice(0, i) + String(str).slice(i + 2);
+        i++;
+      }
+      i++;
+    }
+  }
+  if (String(str).length > 1 && wordCount !== String(str).length) {
+    counter = String(str).length
+    return check(str, bracketsConfig)
+  }
+  return String(str).length === 0 ? true : false
 }
+
+module.exports = check;
+
